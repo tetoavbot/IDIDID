8c39bc1d6b99f8c0a6a5a4baf75dab2f012dfb78
tetoavbot <141924154+tetoavbot@users.noreply.github.com>
2023-09-16 01:33:54 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["G_7_Rr"]
+OWNER_NAME = "teto"
+BOT_TOKEN = "6432841340:AAFmzvjpBRc025leCsXzqafV7N1TLT_VLCc"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/CH_NR"
+GROUP = "https://t.me/t7_AU"
+VIDEO = "https://graph.org/file/061951daec85b0f94b83c.jpg"
+LOGS = "zombie2"
+
